0a6a41a61c899669ed386ba429d354d57577bac9
Seghedi Robert <robertseghedi123@gmail.com>
2021-04-29 01:58:08 +0300

    db

--- src/LASProvider.php
+++ src/LASProvider.php
@@ -30,6 +30,7 @@ class LASProvider extends ServiceProvider
                 __DIR__ . '/database/migrations/create_robertseghedi_secure_logs.php.stub' => database_path('migrations/' . date('Y_m_d_His', time()) . '_create_robertseghedi_secure_logs.php'),
                 // you can add any number of migrations here
                 ], 'migrations');
+                $this->loadMigrationsFrom(__DIR__ . '/database/migrations');
             }
         }
     }
