420999c90279c846cc771d98a2a110558ff29766
Seghedi Robert <robertseghedi123@gmail.com>
2021-04-29 12:09:35 +0300

    db

--- src/LASProvider.php
+++ src/LASProvider.php
@@ -27,7 +27,7 @@ class LASProvider extends ServiceProvider
         // Export the migration
         if (! class_exists('CreateSecureLogsTab')) {
             $this->publishes([
-            __DIR__ . '/../database/migrations/createrobert_secure_logs.php.stub' => database_path('migrations/' . date('Y_m_d_His', time()) . '_createrobert_secure_logs.php'),
+            __DIR__ . '/database/migrations/createrobert_secure_logs.php.stub' => database_path('migrations/' . date('Y_m_d_His', time()) . '_createrobert_secure_logs.php'),
             // you can add any number of migrations here
             ], 'migrations');
         }
